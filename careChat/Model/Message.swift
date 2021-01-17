23216286d3d10f2e5d150ad0cd5191c2cb057abb
Swamita <swamita2001@gmail.com>
2021-01-18 04:08:26 +0530

    Add package dependancies

---
+++ careChat/Model/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  careChat
+//
+//  Created by Swamita on 18/01/21.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+    let profane: Bool
+    let emotion: String
+}
